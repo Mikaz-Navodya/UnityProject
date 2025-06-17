55c72f3b88c7dd89ce6a3260876c577313fb8de4
Mikaz Navodya <mikaznavodya@gmail.com>
2025-06-17 16:31:34 +0530

    Shooting System

--- Assignment/Assets/Scripts/PlayerController.cs
+++ Assignment/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     private Rigidbody2D body;
     [SerializeField]private float speed;
+    private bool isJumping;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
@@ -14,16 +15,45 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
-    {   
-        body.linearVelocity = new Vector2 (Input.GetAxis("Horizontal")*speed, body.linearVelocityY);
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        if (Input.GetKey(KeyCode.Space))
-        {body.linearVelocity=new Vector2(body.linearVelocityX, speed);
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2 (horizontalInput * speed, body.linearVelocityY);
+
+
+        animator.SetBool("IsRunning", horizontalInput!=0);
+        animator.SetBool("IsJumping", isJumping);
+
+        //flip player when moving left right
+        if (horizontalInput > 0.01f) {
+            transform.localScale = new Vector3(0.3f,0.3f,1);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-0.3f,0.3f,1);
+        }
+        if (Input.GetKey(KeyCode.Space)&&(isJumping==false))
+        {
+            Jump();
         }
      //   Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"),0.0f,0.0f);
      //   transform.position = transform.position + horizontal * Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocityX, speed);
+        isJumping = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.gameObject.tag=="Ground"))
+        {
+            isJumping = false;
+        }
     }
 }
